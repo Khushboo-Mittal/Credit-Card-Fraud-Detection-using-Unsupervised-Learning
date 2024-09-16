3302d65912bc97588223846bfd14682168ce4af5
Prachi Tavse <122221756+prachitavse@users.noreply.github.com>
2024-09-17 02:09:49 +0530

    Update ingest.py (updated according to data and updated description)

--- Code/ingest.py
+++ Code/ingest.py
@@ -1,19 +1,18 @@
 # META DATA - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
     # Developer details: 
-        # Name: Mohini T and Vansh R
+        # Name: Prachi and Harshita
         # Role: Architects
         # Code ownership rights: Mohini T and Vansh R
     # Version:
         # Version: V 1.0 (11 July 2024)
-            # Developers: Mohini T and Vansh R
+            # Developers: Prachi and Harshita
             # Unit test: Pass
             # Integration test: Pass
      
     # Description: This code snippet ingests customer data from a CSV file, preprocesses it, and stores it in
-    # PostgreSQL and Cassandra databases.
+    # PostgreSQL database.
         # PostgreSQL: Yes 
-        # Cassandra: Yes
 
 # CODE - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
@@ -25,13 +24,13 @@
 import pandas as pd # Importing pandas for data manipulation
 import db_utils # Importing utility functions for database operations
 
-def ingest_data(data_path, postgres_username, postgres_password, postgres_host, postgres_port, postgres_database, cassandra_host, cassandra_port, cassandra_keyspace):
+def ingest_data(data_path, postgres_username, postgres_password, postgres_host, postgres_port, postgres_database):
     
     data = pd.read_csv(data_path) # Read data from CSV file
 
     # Separate data for PostgreSQL and Cassandra (will have to change this)
-    postgres_data = data[['customer_id', 'subscription_type', 'annual_fee', 'payment_method', 'account_age', 'number_of_logins',
-                          'total_spent', 'num_tickets_raised', 'avg_response_time', 'satisfaction_score', 'country', 'device', 'churn']]
+    postgres_data = data[['transaction_id', 'transaction_date', 'transaction_amount', 'merchant_category', 'card_type', 'transaction_location',
+                          'cardholder_age', 'cardholder_gender', 'transaction_description', 'account_balance', 'alander_income']]
     
     
     # Connect to PostgreSQL
